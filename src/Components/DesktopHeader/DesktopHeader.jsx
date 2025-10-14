392c5e39b92bae3f627cfd62c5957128c3fc5a0d
CharlieAbbott1410 <CharlieAbbott142007@gmail.com>
2025-10-14 15:16:20 +0100

    Finished Hero Banner

---
+++ src/Components/DesktopHeader/DesktopHeader.jsx
@@ -0,0 +1,9 @@
+import "./DesktopHeader.css"
+
+export default function DesktopHeader(){
+    return(
+        <header className="desktop-header">
+            Header Fings
+        </header>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import "./home.css"
 import HeroBanner from "../../Components/HeroBanner/HeroBanner"
+import HomeBannerImage from "../../assets/hero_banner.png"
 
 export default function Home(){
     return(
         <>
-            <HeroBanner/>
+            <HeroBanner image={HomeBannerImage}>
+                <h1>Unsinkable Gains</h1>
+                <h2>Crash through your fitness goal</h2>
+            </HeroBanner>
         </>
     )
 }
