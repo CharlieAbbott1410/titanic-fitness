8f253f9dca99dc7cb04646950e2b5d1dc5c689f9
CharlieAbbott1410 <CharlieAbbott142007@gmail.com>
2025-11-04 16:55:57 +0000

    Sign In done

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,9 +1,58 @@
 import "./Auth.css"
+import HeroBanner from "../../Components/HeroBanner/HeroBanner"
+import AuthImage1 from "../../assets/AuthBanner_01.png"
+import { useState } from "react"
 
-export default function Auth(){
-    return(
-        <>
-            This is my Auth page
-        </>
+function LoginForm({ toggle, header }) {
+    return (
+        <form className="auth-form">
+            <h1>Unsinkable gains await</h1>
+            <input placeholder="Email" type="text"></input>
+            <input placeholder="Password" type="text"></input>
+            <div className="button-rows">
+                <button className="primary-btn main-btn">LOG IN</button>
+                <div className="page-change">
+                    <h2>Don't have an account?</h2>
+                    <button onClick={toggle}>Register</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+function RegisterForm({ toggle }) {
+    return (
+        <form className="auth-form">
+            <h1>Unsinkable gains await</h1>
+            <input placeholder="Email" type="text"></input>
+            <input placeholder="Password" type="text"></input>
+            <input placeholder="Display Name" type="text"></input>
+            <div className="button-rows">
+                <button className="primary-btn main-btn">REGISTER NOW</button>
+                <div className="page-change">
+                    <h2>Already have an account?</h2>
+                    <button onClick={toggle}>Login</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+
+export default function Auth() {
+
+    //If registerForm is true, dispaly register form, else display login form
+    const [registerForm, setRegisterForm] = useState(true)
+    function toggleForm() {
+        setRegisterForm(!registerForm)
+    }
+
+    return (
+
+        <div className="auth-layout">
+            <HeroBanner image={AuthImage1} />
+            <div className="auth-form-column">
+                {registerForm ? <RegisterForm toggle={toggleForm} /> : <LoginForm toggle={toggleForm} />}
+            </div>
+        </div>
     )
 }
