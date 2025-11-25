569c21bbac9a88dd926d36aaed171dca15bf319f
CharlieAbbott1410 <CharlieAbbott142007@gmail.com>
2025-11-25 16:31:17 +0000

    Updated sign in and register forms

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -5,11 +5,13 @@ import { useState } from "react"
 import { useUser } from "../../contexts/UserContext"
 
 function LoginForm({ toggle, header }) {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
     return (
         <form className="auth-form">
             <h1>Unsinkable gains await</h1>
-            <input placeholder="Email" type="text"></input>
-            <input placeholder="Password" type="text"></input>
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} type="text"></input>
+            <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} type="text"></input>
             <div className="button-rows">
                 <button className="primary-btn main-btn">LOG IN</button>
                 <div className="page-change">
@@ -23,6 +25,10 @@ function LoginForm({ toggle, header }) {
 function RegisterForm({ toggle }) {
     const {user, login} = useUser();
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
     function registerUser(){
         login({
             "username" : "goober",
@@ -32,10 +38,9 @@ function RegisterForm({ toggle }) {
     return (
         <form className="auth-form" onSubmit={registerUser}>
             <h1>Unsinkable Gains Await</h1>
-            {user ? <h1>Logged in user: {user.username}</h1> : null}
-            <input placeholder="Email" type="text"></input>
-            <input placeholder="Password" type="text"></input>
-            <input placeholder="Display Name" type="text"></input>
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} type="text"></input>
+            <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} type="text"></input>
+            <input placeholder="Display Name" value={username} onChange={(e) => setUsername(e.target.value)} type="text"></input>
             <div className="button-rows">
                 <button className="primary-btn main-btn">REGISTER NOW</button>
                 <div className="page-change">
