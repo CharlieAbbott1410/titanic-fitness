09331b3ca821b7a8e290abd93388ce32d4312762
CharlieAbbott1410 <CharlieAbbott142007@gmail.com>
2025-12-05 11:59:52 +0000

    Made the workouts page categories

--- src/pages/Workouts/Workouts.jsx
+++ src/pages/Workouts/Workouts.jsx
@@ -1,9 +1,52 @@
+import { useEffect, useState } from "react"
 import "./Workouts.css"
+import axios from "axios"
 
-export default function Workouts(){
-    return(
+export default function Workouts() {
+    const [selected, setSelected] = useState(0)
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [categories, setCategories] = useState([]);
+
+    function getCategories() {
+        const api_endpoint = "https://wger.de/api/v2/exercisecategory/"
+        axios.get(api_endpoint)
+            .then(res => {
+
+                setCategories(
+                    [
+                        {id: 0, "name": "All"},
+                        ...res.data.results
+                    ]
+                );
+            })
+            .catch(err => {
+                setError(err?.response?.data?.detail || "Error Occured")
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+
+    useEffect(getCategories, []);
+
+    if (loading) {
+        return <p>Loading Filters...</p>
+    }
+    if (error) {
+        return <p>its all jover</p>
+    }
+
+    const filterBtns = categories.map((cat => {
+        return <button className={`Category-Btn ${selected === cat.id ? "active" : null}`} onClick={() => setSelected(cat.id) }>{cat.name}</button>
+    }))
+
+
+    return (
         <>
-            This is my workouts page
+            <div className="Categories-Container">
+                {filterBtns}
+            </div>
         </>
     )
 }
